3b8fc53d70f0aa0acba45a1146771e040755e7d5
TalanvorD <164896317+TalanvorD@users.noreply.github.com>
2024-08-06 09:57:31 -0500

    Troubleshooting Render
    Trying to fix the db hosting error on Render deployment

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static('public'));
 
 app.use(routes);
 
